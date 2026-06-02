31691c8f0fe68217ecf483cb5c7cc740f1a75447
seungwoo <rio970319@gmail.com>
2026-06-02 17:20:30 +0900

    feature(ERP-79): PurchaseOrder 모델링 (#6)

---
+++ src/main/java/org/fallguys/procurementservice/domain/model/PurchaseOrder.java
@@ -0,0 +1,22 @@
+package org.fallguys.procurementservice.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PurchaseOrder {
+    private final String code;
+    private String vendorCode;
+    private PurchaseOrderStatus status;
+    private LocalDate expectedArrivalDate;
+    private String memo;
+    private List<PurchaseOrderLine> lines;
+    private ProcurementOrderCreation creation;
+    private ProcurementOrderApproval approval;
+    private ProcurementOrderReceiving receiving;
+    private ProcurementOrderCancellation cancellation;
+}
