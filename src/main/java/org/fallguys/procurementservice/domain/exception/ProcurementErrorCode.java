39e9e0ef78dba3aef57a71e866116ebafbd7046b
seungwoo <rio970319@gmail.com>
2026-06-10 20:05:32 +0900

    feature(ERP-192): 서비스 구현 (#24)

--- src/main/java/org/fallguys/procurementservice/domain/exception/ProcurementErrorCode.java
+++ src/main/java/org/fallguys/procurementservice/domain/exception/ProcurementErrorCode.java
@@ -20,7 +20,8 @@ public enum ProcurementErrorCode {
     ITEM_INACTIVE("PO-03-03", "비활성 품목이 포함되어 있습니다."),
     WAREHOUSE_INACTIVE("PO-03-04", "비활성 창고입니다."),
     PURCHASE_ORDER_NOT_DRAFT("PO-03-05", "DRAFT 상태인 발주서만 수정할 수 있습니다."),
-    INVALID_QUERY_PARAMETER("PO-03-06", "유효하지 않은 조회 파라미터입니다."),
+    PURCHASE_ORDER_NOT_APPROVED("PO-03-06", "APPROVED 상태인 발주서만 입고 처리할 수 있습니다."),
+    INVALID_QUERY_PARAMETER("PO-03-07", "유효하지 않은 조회 파라미터입니다."),
 
     ITEM_SERVICE_ERROR("PO-07-01", "아이템 서비스 호출에 실패했습니다."),
     INVENTORY_SERVICE_ERROR("PO-07-02", "재고 서비스 호출에 실패했습니다."),
